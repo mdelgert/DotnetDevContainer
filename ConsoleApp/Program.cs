7d9d364a5bf1077d0368ac30bf01587eb45dfa42
Matthew Elgert <mdelgert@yahoo.com>
2025-05-09 20:14:42 -0400

    Remove commented-out loop from Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,10 +5,8 @@ Console.WriteLine($".NET Version: {Environment.Version}");
 Console.WriteLine($"OS Platform: {Environment.OSVersion}");
 Console.WriteLine("================================================");
 
-/*
 for (int i = 0; i < 60; i++)
 {
     Console.WriteLine($"This is message number {i + 1}");
     System.Threading.Thread.Sleep(1000); // Sleep for 1 second
 }
-*/
